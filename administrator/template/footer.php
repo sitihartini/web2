e44b88aa5aaa9aa101bb6afaa0ce88652c387c6b
siti <siti.hartini197@gmail.com>
2017-05-17 11:42:35 +0700

    penerapan view pada halaman administrator

--- administrator/template/footer.php
+++ administrator/template/footer.php
@@ -16,17 +16,17 @@
     <!-- /#wrapper -->
 
     <!-- jQuery -->
-    <script src="assets/js/jquery.min.js"></script>
+    <script src="template/assets/js/jquery.min.js"></script>
 
     <!-- Bootstrap Core JavaScript -->
    
-    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="template/assets/js/bootstrap.min.js"></script>
 
     <!-- Metis Menu Plugin JavaScript -->
-    <script src="assets/js/metisMenu.min.js"></script>
+    <script src="template/assets/js/metisMenu.min.js"></script>
 
     <!-- Custom Theme JavaScript -->
-    <script src="assets/js/sb-admin-2.js"></script>
+    <script src="template/assets/js/sb-admin-2.js"></script>
 
 </body>
 
